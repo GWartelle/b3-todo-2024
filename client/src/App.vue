5a77db4dfa9fb48e83171f134d6503aa8ab93ef4
GWartelle <115559211+GWartelle@users.noreply.github.com>
2024-10-14 11:08:20 +0200

    Intall tailwindcss and daisyui

--- client/src/App.vue
+++ client/src/App.vue
@@ -2,16 +2,9 @@
 
 <template>
   <h1>B3 TODO</h1>
-  <p>You must do these:</p>
+  <p class="font-bold text-center">You must do these:</p>
   <ul>
     <li>This task</li>
     <li>This one too</li>
   </ul>
 </template>
-
-<style scoped>
-h1 {
-  font-size: xx-large;
-  text-align: center;
-}
-</style>
