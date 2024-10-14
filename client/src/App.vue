a91ed89a628c84216848e95a44cb6e0eee3491e4
GWartelle <115559211+GWartelle@users.noreply.github.com>
2024-10-14 11:50:48 +0200

    Test if tailwindcss is working

--- client/src/App.vue
+++ client/src/App.vue
@@ -1,10 +1,5 @@
 <script setup></script>
 
 <template>
-  <h1>B3 TODO</h1>
-  <p class="font-bold text-center">You must do these:</p>
-  <ul>
-    <li>This task</li>
-    <li>This one too</li>
-  </ul>
+  <h1 class="text-center">B3 TODO APP</h1>
 </template>
