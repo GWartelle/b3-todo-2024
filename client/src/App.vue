24f573160d011ec8a10a23f823dca03973566c4c
GWartelle <115559211+GWartelle@users.noreply.github.com>
2024-10-14 16:15:28 +0200

    Add MDS mention to the bottom of the page

--- client/src/App.vue
+++ client/src/App.vue
@@ -6,5 +6,6 @@ import { RouterView } from "vue-router";
   <div class="flex flex-col items-center">
     <h1 class="text-center font-bold text-4xl p-10">B3 TODO APP</h1>
     <RouterView />
+    <p class="fixed bottom-0 right-0 pb-5 pr-10">MyDigitalSchool 2024</p>
   </div>
 </template>
